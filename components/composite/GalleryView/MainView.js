03a56e822ec3187e042f5fadccb21ea6083c3ece
Will Germany <germany.william@gmail.com>
2021-02-22 21:45:40 -0500

    Remove cast video button & disable auto play on mobile test

--- components/composite/GalleryView/MainView.js
+++ components/composite/GalleryView/MainView.js
@@ -1,3 +1,4 @@
+/* global window */
 import React from 'react';
 import { bool, shape, string } from 'prop-types';
 import styled from 'styled-components';
@@ -45,7 +46,7 @@ const MainView = ({ media }) => {
   if (media.isVideo) {
     return (
       <MainViewContainer key={media.src} justify="center" align="center" isVideo>
-        <Video key={media.src} width="100%" height="100%" loop autoPlay muted {...media.mediaProps}>
+        <Video key={media.src} width="100%" height="100%" loop autoPlay={typeof window !== 'undefined' && window.innerWidth > 650} muted disableRemotePlayback {...media.mediaProps}>
           <source src={media.src} type="video/mp4" />
           <track kind="captions" />
         </Video>
