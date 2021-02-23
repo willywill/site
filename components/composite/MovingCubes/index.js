a105978a4b4eac4810cae32292303db40b74e743
Will Germany <germany.william@gmail.com>
2021-02-22 23:20:39 -0500

    Removing component causing mobile issues and deps

--- components/composite/MovingCubes/index.js
+++ components/composite/MovingCubes/index.js
@@ -1,43 +1,43 @@
 /* eslint-disable no-unreachable */
-import React, { useEffect } from 'react';
-import styled from 'styled-components';
-import { breakpoints } from '../../../utils/theme';
-import { Flex } from '../../ui';
-import MovingCubes from './MovingCubesRenderer';
+// import React, { useEffect } from 'react';
+// import styled from 'styled-components';
+// import { breakpoints } from '../../../utils/theme';
+// import { Flex } from '../../ui';
+// import MovingCubes from './MovingCubesRenderer';
 
-const MovingCubesWrapper = styled(Flex)`
-  @media (max-width: ${breakpoints.desktopSmall}) {
-    display: none;
-  }
-`;
+// const MovingCubesWrapper = styled(Flex)`
+//   @media (max-width: ${breakpoints.desktopSmall}) {
+//     display: none;
+//   }
+// `;
 
-const MovingCubesComponent = () => {
-  return null;
+const MovingCubesComponent = () =>
+  // TODO: Remove this component or fix it, it's not being used anymore due to bugs on mobile.
+  null;
 
-  useEffect(() => {
-    const movingCubesRenderer = new MovingCubes();
-    movingCubesRenderer.initialize();
-  }, []);
+// useEffect(() => {
+//   const movingCubesRenderer = new MovingCubes();
+//   movingCubesRenderer.initialize();
+// }, []);
 
-  return (
-    <MovingCubesWrapper
-      width={1}
-      mr={6}
-      mt={5}
-      height="256px"
-      id="cube-container"
-      justify="flex-end"
-      align="center"
-      style={{
-        zIndex: -1,
-        position: 'absolute',
-        top: '-100px',
-        left: '5%',
-        opacity: 0.915,
-      }}
-    />
-  );
-};
+// return (
+//   <MovingCubesWrapper
+//     width={1}
+//     mr={6}
+//     mt={5}
+//     height="256px"
+//     id="cube-container"
+//     justify="flex-end"
+//     align="center"
+//     style={{
+//       zIndex: -1,
+//       position: 'absolute',
+//       top: '-100px',
+//       left: '5%',
+//       opacity: 0.915,
+//     }}
+//   />
+// );
 
 MovingCubesComponent.displayName = 'MovingCubesComponent';
 
